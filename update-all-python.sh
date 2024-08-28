ea62822158130c6405e84a0a91750d0a459e40e4
emil237 <marianatalaat1@gmail.com>
2024-08-28 07:32:17 +0300

    Add files via upload

--- update-all-python.sh
+++ update-all-python.sh
@@ -1,99 +1,64 @@
 #!/bin/sh
 
-#
+echo "Uploaded by Emil Nabil"
+sleep 4
 
-if python --version 2>&1 | grep -q '^Python 3\.'; then
-   echo "You have Python3 image"
-   PYTHON='PY3'
+PYTHON_VERSION=$(python --version 2>&1)
+if echo "$PYTHON_VERSION" | grep -q '^Python 3\.'; then
+    echo "You have Python3"
+    PYTHON='PY3'
+elif echo "$PYTHON_VERSION" | grep -q '^Python 2\.'; then
+    echo "You have Python2"
+    PYTHON='PY2'
 else
-   echo "You have Python2 image"
-   PYTHON='PY2'
+    echo "Python 2 or 3 is required."
+    exit 1
 fi
 
-if [ $PYTHON = "PY3" ]; then
-   opkg update && opkg upgrade
-   opkg install p7zip
-   opkg install wget 
-   opkg install curl  
-   opkg install python3-lxml 
-   opkg install python3-requests  
-   opkg install python3-beautifulsoup4   
-   opkg install python3-cfscrape 
-   opkg install livestreamersrv 
-   opkg install python3-six 
-   opkg install python3-sqlite3 
-   opkg install python3-pycrypto 
-   opkg install f4mdump python3-image  
-   opkg install python3-imaging  
-   opkg install python3-argparse 
-   opkg install python3-multiprocessing
-   opkg install python3-mmap 
-   opkg install python3-ndg-httpsclient  
-   opkg install python3-pydoc 
-   opkg install python3-xmlrpc
-   opkg install python3-certifi 
-   opkg install python3-urllib3 
-   opkg install python3-chardet
-   opkg install python3-pysocks 
-   opkg install python3-js2py 
-   opkg install python3-pillow
-   opkg update
-   opkg install enigma2-plugin-systemplugins-serviceapp
-   opkg install ffmpeg
-   opkg install exteplayer3
-   opkg install gstplayer
-   opkg update
-   opkg install gstreamer1.0-plugins-good
-   opkg install gstreamer1.0-plugins-ugly
-   opkg install gstreamer1.0-plugins-base
-   opkg install gstreamer1.0-plugins-bad
+if command -v apt-get >/dev/null 2>&1; then
+    INSTALL="apt-get install -y"
+    CHECK_INSTALLED="dpkg -l"
+    OS='DreamOS'
+    apt-get update
+elif command -v opkg >/dev/null 2>&1; then
+    INSTALL="opkg install --force-reinstall --force-depends"
+    CHECK_INSTALLED="opkg list-installed"
+    OS='Opensource'
+    opkg update && opkg upgrade
 else
-   opkg update && opkg upgrade
-   opkg update
-   opkg install 
-   opkg install wget 
-   opkg install curl  
-   opkg install hlsdl 
-   opkg install python-lxml 
-   opkg install python-requests 
-   opkg install python-beautifulsoup4 
-   opkg install python-cfscrape 
-   opkg install livestreamer 
-   opkg install vlivestreamersrv 
-   opkg install python-six 
-   opkg install python-sqlite3 
-   opkg install python-pycrypto 
-   opkg install f4mdump 
-   opkg install python-image 
-   opkg install python-imaging 
-   opkg install python-argparse 
-   opkg install python-multiprocessing 
-   opkg install python-mmap 
-   opkg install python-ndg-httpsclient 
-   opkg install python-pydoc python-xmlrpc 
-   opkg install python-certifi 
-   opkg install python-urllib3 
-   opkg install python-chardet 
-   opkg install python-pysocks
-   opkg install enigma2-plugin-systemplugins-serviceapp
-    opkg install ffmpeg
-    opkg install exteplayer3
-    opkg install gstplayer
-    opkg update
-    opkg install gstreamer1.0-plugins-good
-    opkg install gstreamer1.0-plugins-ugly
-    opkg install gstreamer1.0-plugins-base
-    opkg install gstreamer1.0-plugins-bad
+    echo "Unsupported OS"
+    exit 1
+fi
+
+if [ "$PYTHON" = "PY3" ]; then
+    $INSTALL p7zip wget curl python3-lxml python3-requests python3-beautifulsoup4
+    $INSTALL python3-cfscrape livestreamersrv python3-six python3-sqlite3 python3-pycrypto
+    $INSTALL f4mdump python3-image python3-imaging python3-argparse python3-multiprocessing
+    $INSTALL python3-mmap python3-ndg-httpsclient python3-pydoc python3-xmlrpc python3-certifi
+    $INSTALL python3-urllib3 python3-chardet python3-pysocks python3-js2py python3-pillow
+    $INSTALL enigma2-plugin-systemplugins-serviceapp ffmpeg exteplayer3 gstplayer
+    $INSTALL gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-base
+    $INSTALL gstreamer1.0-plugins-bad
+else
+    $INSTALL wget curl hlsdl python-lxml python-requests python-beautifulsoup4
+    $INSTALL python-cfscrape livestreamer vlivestreamersrv python-six python-sqlite3
+    $INSTALL python-pycrypto f4mdump python-image python-imaging python-argparse
+    $INSTALL python-multiprocessing python-mmap python-ndg-httpsclient python-pydoc
+    $INSTALL python-xmlrpc python-certifi python-urllib3 python-chardet python-pysocks
+    $INSTALL enigma2-plugin-systemplugins-serviceapp ffmpeg exteplayer3 gstplayer
+    $INSTALL gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-base
+    $INSTALL gstreamer1.0-plugins-bad
 fi
 
 echo ""
 sync
 echo ""
-echo "#         Enigma TOOLS $version INSTALLED SUCCESSFULLY              #"
+echo "#         Enigma TOOLS INSTALLED SUCCESSFULLY              #"
 echo "**************************************************************"
-echo "#              your Device will RESTART Now                  #"
+echo "#              Your device will restart now                  #"
 echo "**************************************************************"
 reboot
 wait
-sleep 2;
+sleep 2
 exit 0
+
